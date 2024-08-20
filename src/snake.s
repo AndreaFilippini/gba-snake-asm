b045f66993bade3931e9b2c6098a33dcdcef72b5
AndreaFilippini <56972407+AndreaFilippini@users.noreply.github.com>
2024-08-20 20:55:31 +0200

    keys added and apple eaten by the snake

--- src/snake.s
+++ src/snake.s
@@ -21,24 +21,46 @@
 	.equ SNAKE_LOC_Y,	0x02010014
 	.equ APPLE_LOC_X,	0x02020000
 	.equ APPLE_LOC_Y,	0x02020004
-	.equ GAME_SPEED,	0x02020008
-	.equ SPEED_VALUE,	0x100
+	.equ GAME_SPEED,	0x02030000
+	.equ SPEED_VALUE,	0x1
+	.equ KEY_A,		0x01
+	.equ KEY_B,		0x02
+	.equ KEY_RIGHT,		0x10
+	.equ KEY_LEFT,		0x20
+	.equ KEY_UP,		0x40
+	.equ KEY_DOWN,		0x80
 
 main:
-	push {r0, lr}
+	push {r0-r1, lr}
 	bl .random
 	bl .enable_bg
 	bl .load_pal
 	bl .load_all_tile
-	bl .render_walls
 	bl .set_snake_prop
 	bl .spawn_apple
 .loop:
 	@ ;;;;;;;;;;;;;;MAIN GAME;;;;;;;;;;;;;;;
 	bl .vid_sync
-	bl .wait_timer
+	bl .incr_timer
+	ldr r1, = SPEED_VALUE
+	cmp r0, r1
+	ble .loop
+
+	bl .reset_timer
+	bl .key_pressed
+	bl .check_eat
+	cmp r0, #0
+	beq .loop_render
+
+	bl .spawn_apple
+
+	.loop_render:
+	bl .clean_buffer
+	bl .render_walls
+	bl .render_apple
 	bl .snake_render
 	bl .snake_update
+	bl .render_screen
 	b .loop
 
 @ // VBlank timing
@@ -139,7 +161,7 @@ main:
 @ // render tile on screen based on x, y and index
 .render_tile:
 	push {r0-r3, lr}
-	ldr r3, .VRAM_BG0
+	ldr r3, .VRAM_BUFF
 	lsl r0, r0, #1
 	lsl r1, r1, #6
 	add r0, r0, r1
@@ -148,9 +170,35 @@ main:
 	@bl __aeabi_idiv
 	pop {r0-r3, pc}
 
+
+@ // copy from buffer to bg0
+.render_screen:
+	push {r0-r3}
+	ldr r0, .VRAM_BUFF
+	ldr r1, .VRAM_BG0
+	ldr r2, = 0x2000
+	swi 0xB
+	pop {r0-r3}
+	bx lr
+
+@ // clean buffer
+.clean_buffer:
+	push {r0-r3}
+	ldr r0, .VRAM_EMPTY
+	ldr r1, .VRAM_BUFF
+	ldr r2, = 0x2000
+	swi 0xB
+	pop {r0-r3}
+	bx lr
+	
 .text
 .align	2
 	.VRAM_BG0: 	.word VRAM + 0x4000
+	.VRAM_BUFF: 	.word VRAM + 0x8000
+	.VRAM_EMPTY: 	.word VRAM + 0xC000
+	.DMASRC: 	.word REG_BASE + 0xB0
+	.DMADST: 	.word REG_BASE + 0xB4
+	.DMACNT: 	.word REG_BASE + 0xB8
 
 @ // render wall on screen border
 .render_walls:
@@ -198,7 +246,7 @@ main:
 	.RANDOM_VAL: 	.word RANDOM
 	.SEED:		.word 0x41C64E61
 
-@ // spawn apple tile
+@ // get new random apple location 
 .spawn_apple:
 	push {r0-r3, lr}
 	bl .random
@@ -217,25 +265,62 @@ main:
 	ldr r3, .APPLE_Y
 	str r0, [r2]
 	str r1, [r3]
-
-	mov r2, #2
-	bl .render_tile
 	pop {r0-r3, pc}
 
+@ // spawn apple tile
+.render_apple:
+	push {r0-r2, lr}
+	ldr r0, .APPLE_X
+	ldr r1, .APPLE_Y
+	ldr r0, [r0]
+	ldr r1, [r1]
+	mov r2, #2
+	bl .render_tile
+	pop {r0-r2, pc}
+
+@ // check if apple was eaten by the snake
+.check_eat:
+	push {r1-r4}
+	mov r4, #0
+	ldr r0, .APPLE_X
+	ldr r1, .APPLE_Y
+	ldr r2, .SNAKE_X
+	ldr r3, .SNAKE_Y
+	ldr r0, [r0]
+	ldr r1, [r1]
+	ldr r2, [r2]
+	ldr r3, [r3]
+	cmp r0, r2
+	bne .check_eat_return
+	cmp r1, r3
+	bne .check_eat_return
+	mov r4, #1
+	.check_eat_return:
+	mov r0, r4
+	pop {r1-r4}
+	bx lr
 .text
 .align	2
 	.APPLE_X: 	.word APPLE_LOC_X
 	.APPLE_Y:	.word APPLE_LOC_Y
 
-@ // wait for the timer before updating the frame
-.wait_timer:
-	push {r0-r2, lr}
-	ldr r1, = SPEED_VALUE
-	.speed_loop:
-		sub r1, r1, #1
-		cmp r1, #0
-		bne .speed_loop
-	pop {r0-r2, pc}
+@ // increment timer speed
+.incr_timer:
+	push {r1}
+	ldr r1, .SPEED
+	ldrh r0, [r1]
+	add r0, r0, #1
+	strh r0, [r1]
+	pop {r1}
+	bx lr
+
+@ // reset timer speed
+.reset_timer:
+	push {r0, lr}
+	ldr r0, .SPEED
+	mov r1, #0
+	str r1, [r0]
+	pop {r0, pc}
 
 @ // update snake position
 .snake_update:
@@ -300,6 +385,74 @@ main:
 	str r1, [r0]
 	pop {r0-r3, pc}
 
+@ // manage key pression
+.key_pressed:
+	push {r0-r4, lr}
+	ldr r2, .DIR_X
+	ldr r3, .DIR_Y
+	ldr r0, .KEY
+	ldrb r0, [r0]
+	add r0, r0, #1
+	neg r0, r0
+	mov r1, #KEY_UP
+	and r1, r0
+	cmp r1, #0
+	bne .key_pressed_up
+	mov r1, #KEY_DOWN
+	and r1, r0
+	cmp r1, #0
+	bne .key_pressed_down
+	mov r1, #KEY_RIGHT
+	and r1, r0
+	cmp r1, #0
+	bne .key_pressed_right
+	mov r1, #KEY_LEFT
+	and r1, r0
+	cmp r1, #0
+	bne .key_pressed_left
+	b .key_pressed_return
+
+	.key_pressed_up:
+	ldr r4, [r3]
+	cmp r4, #0
+	bne .key_pressed_return
+	mov r0, #0
+	mov r1, #1
+	neg r1, r1
+	b .key_pressed_dir
+
+	.key_pressed_down:
+	ldr r4, [r3]
+	cmp r4, #0
+	bne .key_pressed_return
+	mov r0, #0
+	mov r1, #1
+	b .key_pressed_dir
+
+	.key_pressed_right:
+	ldr r4, [r2]
+	cmp r4, #0
+	bne .key_pressed_return
+	mov r0, #1
+	mov r1, #0
+	b .key_pressed_dir
+	
+	.key_pressed_left:
+	ldr r4, [r2]
+	cmp r4, #0
+	bne .key_pressed_return
+	mov r0, #1
+	neg r0, r0
+	mov r1, #0
+	b .key_pressed_dir
+
+	.key_pressed_dir:
+	str r0, [r2]
+	str r1, [r3]
+
+	.key_pressed_return:
+	pop {r0-r4, pc}
+
 .text
 .align	2
 	.SNAKE_X: 	.word SNAKE_LOC_X
@@ -309,3 +462,4 @@ main:
 	.DIR_X:		.word SNAKE_DIR_X
 	.DIR_Y:		.word SNAKE_DIR_Y
 	.SPEED:		.word GAME_SPEED
+	.KEY:		.word REG_BASE + 0x130
