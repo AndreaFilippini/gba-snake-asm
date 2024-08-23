4a3f1fa94ec9d7ac1829da6845e581a5b67cf9e3
AndreaFilippini <56972407+AndreaFilippini@users.noreply.github.com>
2024-08-23 10:42:16 +0200

    snake code finished

--- src/snake.s
+++ src/snake.s
@@ -13,6 +13,8 @@
 	.equ TOTAL_TILE,	0x4
 	.equ DISPLAY_WIDTH,	240
 	.equ DISPLAY_HEIGHT,	160
+	.equ DEF_MAX_LEN,	200
+	.equ DEF_INIT_LEN,	5
 	.equ MAX_LENGTH,	0x02010000
 	.equ SNAKE_LENGTH,	0x02010004
 	.equ SNAKE_DIR_X,	0x02010008
@@ -22,7 +24,8 @@
 	.equ APPLE_LOC_X,	0x02020000
 	.equ APPLE_LOC_Y,	0x02020004
 	.equ GAME_SPEED,	0x02030000
-	.equ SPEED_VALUE,	0x1
+	.equ PROC_INPUT,	0x02030004
+	.equ SPEED_VALUE,	0x3
 	.equ KEY_A,		0x01
 	.equ KEY_B,		0x02
 	.equ KEY_RIGHT,		0x10
@@ -32,6 +35,7 @@
 
 main:
 	push {r0-r1, lr}
+.reset:	
 	bl .random
 	bl .enable_bg
 	bl .load_pal
@@ -41,27 +45,45 @@ main:
 .loop:
 	@ ;;;;;;;;;;;;;;MAIN GAME;;;;;;;;;;;;;;;
 	bl .vid_sync
+	bl .key_pressed
 	bl .incr_timer
 	ldr r1, = SPEED_VALUE
 	cmp r0, r1
 	ble .loop
 
 	bl .reset_timer
-	bl .key_pressed
 	bl .check_eat
 	cmp r0, #0
 	beq .loop_render
 
+	bl .add_snake_block
 	bl .spawn_apple
 
 	.loop_render:
 	bl .clean_buffer
-	bl .render_walls
 	bl .render_apple
 	bl .snake_render
 	bl .snake_update
+	bl .render_walls
 	bl .render_screen
+	bl .reset_processed_input
+
+	ldr r0, .SNAKE_X
+	ldr r1, .SNAKE_Y
+	ldr r0, [r0]
+	ldr r1, [r1]
+	bl .get_tile_index
+	cmp r0, #1
+	beq .end_game
+	bl .snake_eat_itself
+	cmp r0, #1
+	beq .end_game
 	b .loop
+
+.end_game:
+	bl .clean_snake_blocks
+	b .reset
+	pop {r0-r1, pc}
 
 @ // VBlank timing
 .vid_sync:
@@ -100,8 +122,8 @@ main:
 	ldr r0, .PALETTE
 	ldr r1, .CLR_WHITE
 	ldr r2, .CLR_RED
-	ldr r3, .CLR_BLUE
-	ldr r4, .CLR_GREEN
+	ldr r3, .CLR_GREEN
+	ldr r4, .CLR_GREEN_W
 	strh r1, [r0]
 	strh r2, [r0, #2]
 	strh r3, [r0, #4]
@@ -113,8 +135,8 @@ main:
 	.PALETTE: 	.word PLTT + 0x2
 	.CLR_WHITE:	.word (31 | (31 << 5) | (31 << 10))
 	.CLR_RED:	.word (28 | (10 << 5) | (10 << 10))
-	.CLR_BLUE:	.word (10 | (28 << 5) | (10 << 10))
-	.CLR_GREEN:	.word (10 | (10 << 5) | (28 << 10))
+	.CLR_GREEN_W:	.word (8 | (24 << 5) | (8 << 10))
+	.CLR_GREEN:	.word (10 | (28 << 5) | (10 << 10))
 
 @ // load number of tiles specified
 .load_all_tile:
@@ -170,6 +192,17 @@ main:
 	@bl __aeabi_idiv
 	pop {r0-r3, pc}
 
+@ // get tile on screen based on x and y
+.get_tile_index:
+	push {r1-r3}
+	ldr r3, .VRAM_BUFF
+	lsl r0, r0, #1
+	lsl r1, r1, #6
+	add r0, r0, r1
+	add r0, r0, r3
+	ldrh r0, [r0]
+	pop {r1-r3}
+	bx lr
 
 @ // copy from buffer to bg0
 .render_screen:
@@ -322,23 +355,98 @@ main:
 	str r1, [r0]
 	pop {r0, pc}
 
+@ // add new snake block
+.add_snake_block:
+	push {r0-r6, lr}
+	ldr r0, .SNAKE_LEN
+	ldr r1, [r0]
+	add r1, r1, #1
+	str r1, [r0]
+	pop {r0-r6, pc}
+
+@ // check if the snake eats itself
+.snake_eat_itself:
+	push {r1-r7}
+	mov r7, #1
+	ldr r0, .SNAKE_X
+	ldr r1, .SNAKE_Y
+	ldr r2, .SNAKE_LEN
+	ldr r2, [r2]
+	ldr r3, [r0]
+	ldr r4, [r1]
+
+	.snake_eat_itself_loop:
+		add r0, r0, #8
+		add r1, r1, #8
+		ldr r5, [r0]
+		ldr r6, [r1]
+		cmp r3, r5
+		bne .snake_eat_itself_incr
+		cmp r4, r6
+		beq .snake_eat_itself_return
+
+		.snake_eat_itself_incr:
+		sub r2, r2, #1
+		cmp r2, #0
+		bne .snake_eat_itself_loop
+
+	.snake_eat_itself_no:
+	mov r7, #0
+	
+	.snake_eat_itself_return:
+	mov r0, r7
+	pop {r1-r7}
+	bx lr
+
+@ // clean snake blocks locations x and y from the memory
+.clean_snake_blocks:
+	push {r0-r3, lr}
+	ldr r0, .SNAKE_X
+	ldr r1, .SNAKE_Y
+	ldr r2, .SNAKE_LEN
+	ldr r2, [r2]
+	mov r3, #0
+	.clean_snake_blocks_loop:
+		str r3, [r0]
+		str r3, [r1]
+		add r0, r0, #8
+		add r1, r1, #8
+		sub r2, r2, #1
+		cmp r2, #0
+		bne .clean_snake_blocks_loop
+	pop {r0-r3, pc}
+
 @ // update snake position
 .snake_update:
 	push {r0-r6, lr}
 	ldr r0, .SNAKE_X
-	ldr r1, .DIR_X
-	ldr r2, [r0]
-	ldr r3, [r1]
-	add r2, r2, r3
-	str r2, [r0]
+	ldr r1, .SNAKE_Y
+	ldr r2, .SNAKE_LEN
+	ldr r2, [r2]
+	.snake_update_loop:
+		sub r2, r2, #1
+		cmp r2, #0
+		beq .snake_update_set
+		sub r3, r2, #1
+		lsl r4, r2, #3
+		lsl r3, r3, #3
+		ldr r5, [r0, r3]
+		ldr r6, [r1, r3]
+		str r5, [r0, r4]
+		str r6, [r1, r4]
+		b .snake_update_loop
 
-	ldr r0, .SNAKE_Y
-	ldr r1, .DIR_Y
-	ldr r2, [r0]
+	.snake_update_set:
+	ldr r2, .DIR_X
+	ldr r3, [r0]
+	ldr r4, [r2]
+	add r3, r3, r4
+	str r3, [r0]
+	ldr r2, .DIR_Y
 	ldr r3, [r1]
-	add r2, r2, r3
-	str r2, [r0]
-
+	ldr r4, [r2]
+	add r3, r3, r4
+	str r3, [r1]
 	pop {r0-r6, pc}
 
 @ // show snake on screen
@@ -346,13 +454,22 @@ main:
 	push {r0-r6, lr}
 	ldr r5, .SNAKE_X
 	ldr r6, .SNAKE_Y
-	mov r2, #3
 	ldr r3, .SNAKE_LEN
 	ldr r3, [r3]
 	mov r4, #0
 	.snake_render_loop:
 		ldr r0, [r5]
 		ldr r1, [r6]
+		cmp r4, #0
+		bne .snake_body_render
+
+		mov r2, #4
+		b .snake_render_call
+
+		.snake_body_render:
+		mov r2, #3
+
+		.snake_render_call:
 		bl .render_tile
 		add r5, r5, #8
 		add r6, r6, #8
@@ -363,31 +480,52 @@ main:
 
 @ // set initial snake properties
 .set_snake_prop:
-	push {r0-r3, lr}
-	mov r0, #((DISPLAY_WIDTH / 8) / 2)
-	mov r1, #((DISPLAY_HEIGHT / 8) / 2)
-	ldr r2, .SNAKE_X
-	ldr r3, .SNAKE_Y
-	str r0, [r2]
-	str r1, [r3]
-	ldr r2, .SNAKE_LEN
-	mov r0, #1
-	str r0, [r2]
-	ldr r2, .MAX_LEN
-	mov r0, #5
-	str r0, [r2]
-
+	push {r0-r6, lr}
 	ldr r0, .DIR_X
 	mov r1, #1
 	str r1, [r0]
 	ldr r0, .DIR_Y
 	mov r1, #0
 	str r1, [r0]
-	pop {r0-r3, pc}
+
+	mov r0, #((DISPLAY_WIDTH / 8) / 2)
+	mov r1, #((DISPLAY_HEIGHT / 8) / 2)
+
+	ldr r2, .SNAKE_X
+	ldr r3, .SNAKE_Y
+	str r0, [r2]
+	str r1, [r3]
+
+	ldr r5, .SNAKE_LEN
+	mov r4, #DEF_INIT_LEN
+	str r4, [r5]
+	mov r5, #0
+	.create_snake_blocks:
+		lsl r6, r5, #3
+		str r0, [r2, r6]
+		str r1, [r3, r6]
+		add r5, r5, #1
+		cmp r5, r4
+		bne .create_snake_blocks
+
+	pop {r0-r6, pc}
+
+@ // reset processed input
+.reset_processed_input:
+	push {r0, lr}
+	ldr r0, .PROCESSED
+	mov r1, #0
+	str r1, [r0]
+	pop {r0, pc}
 
 @ // manage key pression
 .key_pressed:
-	push {r0-r4, lr}
+	push {r0-r5, lr}
+	ldr r5, .PROCESSED
+	ldr r1, [r5]
+	cmp r1, #0
+	bne .key_pressed_return
+
 	ldr r2, .DIR_X
 	ldr r3, .DIR_Y
 	ldr r0, .KEY
@@ -449,17 +587,19 @@ main:
 	.key_pressed_dir:
 	str r0, [r2]
 	str r1, [r3]
+	mov r1, #1
+	str r1, [r5]
 
 	.key_pressed_return:
-	pop {r0-r4, pc}
+	pop {r0-r5, pc}
 
 .text
 .align	2
 	.SNAKE_X: 	.word SNAKE_LOC_X
 	.SNAKE_Y:	.word SNAKE_LOC_Y
-	.MAX_LEN:	.word MAX_LENGTH
 	.SNAKE_LEN:	.word SNAKE_LENGTH
 	.DIR_X:		.word SNAKE_DIR_X
 	.DIR_Y:		.word SNAKE_DIR_Y
 	.SPEED:		.word GAME_SPEED
 	.KEY:		.word REG_BASE + 0x130
+	.PROCESSED:	.word PROC_INPUT
